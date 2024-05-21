162617982fbb682d198e0118c450cd1c7aee9eb6
Hoang Le Duc <leduchoang011@gmail.com>
2024-05-22 03:40:12 +0700

    Solved lab

---
+++ sql-subqueries.sql
@@ -0,0 +1,100 @@
+-- Challenge
+-- Write SQL queries to perform the following tasks using the Sakila database:
+
+-- Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+SELECT COUNT(*) AS num_copies
+FROM inventory
+JOIN film ON inventory.film_id = film.film_id
+WHERE film.title = 'Hunchback Impossible';
+
+-- List all films whose length is longer than the average length of all the films in the Sakila database.
+SELECT title, length
+FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+-- Use a subquery to display all actors who appear in the film "Alone Trip".
+SELECT actor.first_name, actor.last_name
+FROM actor
+WHERE actor_id IN (
+    SELECT actor_id
+    FROM film_actor
+    WHERE film_id = (
+        SELECT film_id
+        FROM film
+        WHERE title = 'Alone Trip'
+    )
+);
+
+-- Bonus:
+
+-- Sales have been lagging among young families, and you want to target family movies for a promotion. 
+-- Identify all movies categorized as family films.
+SELECT f.title
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category cat ON fc.category_id = cat.category_id
+WHERE cat.name = 'Family';
+
+-- Retrieve the name and email of customers from Canada using both subqueries and joins. 
+-- To use joins, you will need to identify the relevant tables and their primary and foreign keys.
+SELECT customer.first_name, customer.last_name, customer.email
+FROM customer
+JOIN address ON customer.address_id = address.address_id
+JOIN city ON address.city_id = city.city_id
+WHERE city.city_id IN (
+    SELECT city_id
+    FROM city
+    WHERE country_id = (
+        SELECT country_id
+        FROM country
+        WHERE country = 'Canada'
+    )
+);
+
+
+-- Determine which films were starred by the most prolific actor in the Sakila database. 
+-- A prolific actor is defined as the actor who has acted in the most number of films.
+--  First, you will need to find the most prolific actor and then use that actor_id to find the different films that he or she starred in.
+
+-- Use the actor_id from the query to find the films
+SELECT film.title
+FROM film
+JOIN film_actor ON film.film_id = film_actor.film_id
+WHERE film_actor.actor_id = (
+    SELECT actor_id
+    FROM film_actor
+    GROUP BY actor_id
+    ORDER BY COUNT(film_id) DESC
+    LIMIT 1
+);
+-- Find the films rented by the most profitable customer in the Sakila database. 
+-- You can use the customer and payment tables to find the most profitable customer, i.e., the customer who has made the largest sum of payments.
+
+SELECT film.title
+FROM rental
+JOIN inventory ON rental.inventory_id = inventory.inventory_id
+JOIN film ON inventory.film_id = film.film_id
+WHERE rental.customer_id = (
+    SELECT customer_id
+    FROM payment
+    GROUP BY customer_id
+    ORDER BY SUM(amount) DESC
+    LIMIT 1
+);
+-- Retrieve the client_id and the total_amount_spent of those clients who spent more than the average of the total_amount spent by each client. 
+-- You can use subqueries to accomplish this.
+
+SELECT customer_id, total_amount
+FROM (
+    SELECT customer_id, SUM(amount) AS total_amount
+    FROM payment
+    GROUP BY customer_id
+) AS client_totals
+WHERE total_amount > (
+    SELECT AVG(total_amount) AS avg_total_spent
+    FROM (
+        SELECT customer_id, SUM(amount) AS total_amount
+        FROM payment
+        GROUP BY customer_id
+    ) AS subquery
+);
